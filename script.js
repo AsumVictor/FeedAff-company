ac7188cc687a5c6251dad915cb9f7763f02a0687
AsumVictor <iamasum369@gmail.com>
2022-10-04 18:36:05 +0000

    Arrays of animals

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
 const menuToggler = document.querySelector(".hamburger");
 const menu = document.querySelector("#menu");
+const serviceArea = document.querySelector(".servicesArea");
+
+//Arrays of Home page Services
+ homeServices = [
+     {
+        bigImage: 'images/Hydro_System.png',
+        circleImg: 'images/logo.jpg',
+        title: 'Fresh Vegetables',
+        shortDescription: `We produce healthy, fresh and organic vegatbles. we sell carrot, Cabbage,  
+                            lettuce, tomatoes, onion and ginger `
+     },
+     {
+        bigImage: 'images/turkey.jpg',
+        circleImg: 'images/Turkeybird.webp',
+        title: 'Turkeys',
+        shortDescription: ` We sell Turkeys all types -You can get matured turkeys, poults
+                           male and females at affordable prices. `
+     },
+     {
+     bigImage: 'images/turkey.jpg',
+        circleImg: 'images/Turkeybird.webp',
+        title: 'Turkeys',
+        shortDescription: ` We sell Fowls all types both
+                           male and females at affordable prices. All our produce are free from virus`
+     }
+ ]
 
 menuToggler.addEventListener('click',()=>{
     menuToggler.classList.toggle("open")
     menu.classList.toggle("hidden")
 })
 
+window.addEventListener('load',()=>{
+      
+      homeServices.forEach(homeService => {
+        let serContainer ;
+        const serContent = `
+      <div class="img-div relative w-full flex justify-center items-center" style="height:5cm;">
+      <img src="${homeService.bigImage}" 
+         alt="" 
+         class="w-full h-full absolute">
+    </div>
+    <div class="serviceContent flex flex-col justify-center items-center">
+         <div class="circle bg-red-500 bg-red-500 rounded-full border-4 overflow-hidden
+                    flex items-center justify-center" style="height:2.1cm;width:2.1cm;">
+          <img src="${homeService.circleImg}" alt="${homeService.title} image" class="h-full w-full absolute">
+        </div>
+         <h2 class="-mt-5 text-2xl font-bold text-center">${homeService.title}</h2>
+         <p class="mt-4 text-center p-2">${homeService.shortDescription}</p>
 
+    </div>
+      `
+
+      serContainer = document.createElement('div')
+      serContainer.setAttribute('class','serContainer overflow-hidden rounded-md bg-darkGreen text-white font-bold relative mt-14')
+      serContainer.innerHTML = serContent;
+      serviceArea.appendChild(serContainer)
+
+
+
+      });
+      
+})
